9ef147e8f9ef84cd9458a8c91441b425524ab64a
Eliel Souza <elielsf13@gmail.com>
2024-06-13 16:09:24 -0300

    adição das notificações

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,37 @@
 import './css/App.css'
 import Login from './components/Login/Login.jsx'
 
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 export default function App() {
+  const notify = () => {
+    toast.success('🦄 Wow so easy!', {
+      position: 'top-right',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark',
+    })
+  }
+
   return (
-    <div className="w-screen flex items-center justify-center bg-neutral-400">
+    <div className="w-screen h-screen flex flex-col justify-center items-center bg-neutral-400">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <Login />
     </div>
   )
--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -26,43 +26,58 @@ export default function Login() {
   }
 
   return (
-    <div className="w-screen h-96 bg-white flex flex-col justify-center items-center bg-slate-600">
-      <form
-        className="w-5/6 h-1 flex flex-col justify-center items-center "
-        id="login-form"
-        onSubmit={handleSubmit}
-      >
-        <div className="flex flex-col text-left">
-          <label className="text-white" htmlFor="email-login">
-            Email
-          </label>
-          <input
-            name="email-login"
-            type="email"
-            value={userData.email_user}
-            onChange={(e) => setUserData(e.target.value)}
-            title="Campo de email"
-            required
-          />
-        </div>
-        <div className="w-full flex flex-col text-left">
-          <label className="text-white text-xl " htmlFor="password-login">
-            Senha
-          </label>
-          <input
-            name="password-login"
-            type="password"
-            value={userData.password_user}
-            onChange={(e) => setUserData(e.target.value)}
-            minLength="8"
-            title="Campo de senha"
-            required
-          />
-        </div>
-      </form>
-      <button className="w-full" type="submit" form="login-form">
-        Login
-      </button>
-    </div>
+    <form
+      className="w-5/6 h-80 bg-white flex flex-col justify-center items-center rounded-md"
+      id="login-form"
+      onSubmit={handleSubmit}
+    >
+      <div className="w-full h-40 text-center flex justify-center items-center">
+        <h1 className="text-4xl">Entrar</h1>
+      </div>
+      <div className="w-full flex flex-col text-left mb-2">
+        <label
+          className="text-whitetext-black text-xl ml-7 mb-1"
+          htmlFor="email-login"
+        >
+          Email
+        </label>
+        <input
+          className="border-slate-500 border-2border-black border-2 w-5/6 place-self-center rounded-md pl-1"
+          name="email-login"
+          type="email"
+          value={userData.email_user}
+          onChange={(e) => setUserData(e.target.value)}
+          title="Campo de email"
+          required
+        />
+      </div>
+      <div className="w-full flex flex-col text-left">
+        <label
+          className="text-black text-xl ml-7 mb-1"
+          htmlFor="password-login"
+        >
+          Senha
+        </label>
+        <input
+          className="border-slate-500 border-2 w-5/6 place-self-center rounded-md"
+          name="password-login"
+          type="password"
+          value={userData.password_user}
+          onChange={(e) => setUserData(e.target.value)}
+          minLength="8"
+          title="Campo de senha"
+          required
+        />
+      </div>
+      <div className="w-full h-4/5 flex justify-center items-center">
+        <button
+          className="w-1/2 h-1/3 bg-violet-700 text-xl text-white rounded-md"
+          type="submit"
+          form="login-form"
+        >
+          Login
+        </button>
+      </div>
+    </form>
   )
 }
